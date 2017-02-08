de768d5958bdefc32646100609b9e5a765b9505f
rbclemen <rbclemen@oakland.edu>
2017-02-08 12:52:10 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,16 +1,28 @@
 package Game;
+import java.util.ArrayList;
+
 ListBox fridge;
 ListBox lunchBox;
 
-public class Game(ListBox fridgeRef,ListBox lunchBoxRef) {
-    fridge = fridgeRef;
-    lunchBox = lunchBoxRef;
-  
+public class Game() {
+    ListBox fridge;
+    ListBox lunchBox;
+    
+    public game(ListBox fridgeRef,ListBox lunchBoxRef) {
+        fridge = fridgeRef;
+        lunchBox = lunchBoxRef;
+    }
     public void add() {
-      
+        if (lunchBox.getItemList().getSelectedIndex() != -1 && lunchBox.getModel().size() > 0) {
+					fridge.addItem(lunchBox.getSelectedItem());
+					lunchBox.removeSelectedItem();
+        }
     }
     public void remove() {
-    
+         if (lunchBox.getItemList().getSelectedIndex() != -1 && lunchBox.getModel().size() > 0) {
+					fridge.addItem(lunchBox.getSelectedItem());
+					lunchBox.removeSelectedItem();
+         }
     }
     public void pack() {
       
