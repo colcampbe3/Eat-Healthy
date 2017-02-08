f30c70cd9e6447204ddb51e211ccc3932677326a
rbclemen <rbclemen@oakland.edu>
2017-02-08 12:36:56 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,19 @@
+package Game;
+ListBox fridge;
+ListBox lunchBox;
 
+public class Game(ListBox fridgeRef,ListBox lunchBoxRef) {
+    fridge = fridgeRef;
+    lunchBox = lunchBoxRef;
+  
+    public void add() {
+      
+    }
+    public void remove() {
+    
+    }
+    public void pack() {
+      
+    }
+  
+}
